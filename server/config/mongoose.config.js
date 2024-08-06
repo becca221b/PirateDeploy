6a1a5cd5679cba135a09864c17c439708493e5cc
becca221b <lrpmqrp@gmail.com>
2024-08-06 01:46:48 -0300

    mongoose config

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,7 +1,25 @@
 const mongoose = require('mongoose');
+const dbURI = process.env.MONGODB_URI;
+/*
 mongoose.connect("mongodb://localhost/pirates_DB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
     .then(() => console.log("Established a connection to the database"))
     .catch(err => console.log("Something went wrong when connecting to the database", err));
+*/
+
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+    console.log('Connected to MongoDB');
+})
+    .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+      
+   
