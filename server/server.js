2659481d74201c23f5f104bde95b2fc30784d956
becca221b <lrpmqrp@gmail.com>
2024-08-06 00:27:20 -0300

    update code to use the env variable

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ const port = process.env.PORT || 8000
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const mongoose = require('mongoose');
+
+const dbURI = process.env.MONGODB_URI;
 const createAdminAccount = require('./scripts/admin');
+
 require('./config/mongoose.config')
 app.use(cors())
 app.use(express.json());
@@ -12,6 +16,17 @@ createAdminAccount();
 
 require('./routes/pirates.routes')(app);
 require('./routes/user.router')(app)
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 app.listen(port, () => {
     console.log(`Listening at port ${port}`)
