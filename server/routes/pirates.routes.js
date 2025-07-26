d8707a8162abe3e0214e9839386a4a9aa6d53b9a
becca221b <86609729+becca221b@users.noreply.github.com>
2025-07-26 16:52:43 -0300

    Update pirates main route

--- server/routes/pirates.routes.js
+++ server/routes/pirates.routes.js
@@ -1,7 +1,7 @@
 const PirateController = require("../controllers/pirate.controller");
 
 module.exports = app => {
-    app.get("/api/pirates/", PirateController.findAllPirates);
+    app.get("/api/pirates", PirateController.findAllPirates);
     app.get("/api/pirate/:id", PirateController.findOnePirate);
     app.put("/api/pirate/:id", PirateController.updateExistingPirate);
     app.post("/api/pirate/new", PirateController.createPirate);
